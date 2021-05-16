e07ff30e23b7edc8708b84b2ed5558ba2bd8b264
narin <narin1101@gmail.com>
2021-05-16 15:23:47 +0900

    replaceerror

--- app/src/main/java/com/google/sample/cloudvision/SQLiteOpenHelper.java
+++ app/src/main/java/com/google/sample/cloudvision/SQLiteOpenHelper.java
@@ -3,12 +3,11 @@ package com.google.sample.cloudvision;
 //public class SQLiteOpenHelper {
 //}
 
-import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 class ProductDBHelper extends SQLiteOpenHelper {  //새로 생성한 adapter 속성은 SQLiteOpenHelper이다.
-    public ProductDBHelper(Context context, String DBname) {
+    public ProductDBHelper(Fragment1 context, String DBname) {
         super(context, DBname, null, 1);    // db명과 버전만 정의 한다.
         //super(context, "forthisType.db", null, 1);
         // TODO Auto-generated constructor stub
