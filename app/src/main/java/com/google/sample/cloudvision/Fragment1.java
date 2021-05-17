509bde8fdccf6d182cbb23fcbbdd299bdb232f2c
hmson18 <sondo100@hanmail.net>
2021-05-18 00:02:29 +0900

    basicJavaXmlAdd

---
+++ app/src/main/java/com/google/sample/cloudvision/Fragment1.java
@@ -0,0 +1,22 @@
+package com.google.sample.cloudvision;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment1, container, false); //container가 뭐지?
+    }
+
+    /*
+    //교재309p, 299p 참조 mainactivity
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment1, container, false);
+        image
+     */
+}
